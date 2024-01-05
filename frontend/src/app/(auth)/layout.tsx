75e797def4248b72dc3153fe076b4f2131ae2024
Pike-Dzurny <pike.m.dzurny@gmail.com>
2024-01-05 15:48:46 -0600

    Remove unnecessary HTML tag from layout.tsx

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -27,7 +27,6 @@ function RootLayout({
 
     <QueryClientProvider client={queryClient}>
     <Head>
-    <html lang="en" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     </Head>
 
